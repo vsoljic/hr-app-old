69736745b70bad3c5a42dc135442ca2995115703
Vedrana Šoljić <vedrana.soljic@king-ict.hr>
2018-08-24 17:36:34 +0200

    frontend app.module.ts

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,36 +1,31 @@
 import './vendor.ts';
 
-import {Injector, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {LocalStorageService, Ng2Webstorage, SessionStorageService} from 'ngx-webstorage';
-import {JhiEventManager} from 'ng-jhipster';
+import { Injector, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LocalStorageService, Ng2Webstorage, SessionStorageService } from 'ngx-webstorage';
+import { JhiEventManager } from 'ng-jhipster';
 
-import {AuthInterceptor} from './blocks/interceptor/auth.interceptor';
-import {AuthExpiredInterceptor} from './blocks/interceptor/auth-expired.interceptor';
-import {ErrorHandlerInterceptor} from './blocks/interceptor/errorhandler.interceptor';
-import {NotificationInterceptor} from './blocks/interceptor/notification.interceptor';
-import {HrAppSharedModule} from 'app/shared';
-import {HrAppCoreModule} from 'app/core';
-import {HrAppAppRoutingModule} from './app-routing.module';
-import {HrAppHomeModule} from './home/home.module';
-import {HrAppAccountModule} from './account/account.module';
-import {HrAppEntityModule} from './entities/entity.module';
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { HrAppSharedModule } from 'app/shared';
+import { HrAppCoreModule } from 'app/core';
+import { HrAppAppRoutingModule } from './app-routing.module';
+import { HrAppHomeModule } from './home/home.module';
+import { HrAppAccountModule } from './account/account.module';
+import { HrAppEntityModule } from './entities/entity.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
-import {
-    ActiveMenuDirective,
-    ErrorComponent,
-    FooterComponent,
-    JhiMainComponent,
-    NavbarComponent,
-    PageRibbonComponent
-} from './layouts';
-
+import { ActiveMenuDirective, ErrorComponent, FooterComponent, JhiMainComponent, NavbarComponent, PageRibbonComponent } from './layouts';
+import { NgbDateCustomParserFormatter } from 'app/shared/ngb-date-custom-parser-formatter';
+import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { GoalGroupComponent } from './goal-group/goal-group.component';
 @NgModule({
     imports: [
         BrowserModule,
         HrAppAppRoutingModule,
-        Ng2Webstorage.forRoot({prefix: 'jhi', separator: '-'}),
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-' }),
         HrAppSharedModule,
         HrAppCoreModule,
         HrAppHomeModule,
@@ -38,8 +33,15 @@ import {
         HrAppEntityModule
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
-    declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective,
-        FooterComponent],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent,
+        GoalGroupComponent
+    ],
     providers: [
         {
             provide: HTTP_INTERCEPTORS,
@@ -64,9 +66,12 @@ import {
             useClass: NotificationInterceptor,
             multi: true,
             deps: [Injector]
+        },
+        {
+            provide: NgbDateParserFormatter,
+            useClass: NgbDateCustomParserFormatter
         }
     ],
     bootstrap: [JhiMainComponent]
 })
-export class HrAppAppModule {
-}
+export class HrAppAppModule {}
